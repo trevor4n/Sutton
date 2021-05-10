25f8e0ebfca686efc3912826fc683f732920af68
trevor4n <trevor4n@gmail.com>
2021-05-10 17:22:29 -0500

    Sutton button html initialization tweak, tweaked info visibility, transitioning game sequence to async

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ let inputPhase = false
 sb.addEventListener('click', () => suttonButton())
 
 for(let i = 0; i < pads.length; i++){
-    pads[i].addEventListener('click', () => padIn(pads[i]))
+    //pads[i].addEventListener('click', () => padIn(pads[i]))
+    pads[i].addEventListener('click', () => padIn(event.currentTarget))
     padCount++
 }
 
@@ -36,23 +37,28 @@ function suttonButton(){
         //let info = document.querySelectorAll('.info')
         //info.forEach((inf) => {console.log(`here ${inf}`); inf.style.visibility = 'visible'})
         ///let info = document.getElementById('highScore').style.visibility = "visible"
-        console.log(`here: ${document.getElementById('highScore').classList}`)
-        document.getElementById('highScore').classList.remove('invisible')
-        console.log(`here: ${document.getElementById('highScore').classList}`)
+        //console.log(`here: ${document.getElementById('highScore').classList}`)
+        document.getElementById('highScore').classList.remove("invisible")
+        //console.log(`here: ${document.getElementById('highScore').classList}`)
         state = 'game-sequence'
     }
     gameSequence()
 }
 
-function gameSequence(){
+async function gameSequence(){
     gameSeq.push(Math.floor(Math.random() * padCount))
     console.log(`Game Sequence: ${gameSeq}`)
     //STRETCH - consider generating a random array of values before gameplay starts to avoid a delay between a successful pad push and the next prompt running
     //gameSeq.from({length: 99}, () => Math.floor(Math.random() * 4))
 
     for(let g of gameSeq){
-        setTimeout(suttonShow(g), 500)
-        setTimeout(suttonSolid, 1000)
+        // setTimeout(suttonShow(g), 750)
+        // setTimeout(suttonSolid, 250)
+        console.log('next game sequence pad to show: ' + 'p'+g)
+        let active = await suttonShow(document.getElementById('p'+g))
+        console.log(active)
+        let solid = await suttonSolid
+        console.log(solid)
     }
     playerSequence()
 }
@@ -63,6 +69,7 @@ function playerSequence(){
     let validatedSeq = 0
     while(validatedSeq < gameSeq.length){
         //TODO - grab padIn code
+        
     }
     inputPhase = false //TODO - possibly move this into a condidional w/n the while loop
     console.log('Player Sequence match')
@@ -72,8 +79,7 @@ function playerSequence(){
 function padIn(p){
     if(inputPhase){
         playerSeq.push(p.id.substring(1)) //in HTML 4, ids must begin with a letter
-        suttonShow(p.id.substring(1))
-        //let padIndex = playerSeq.length-1
+        suttonShow(p)
         if(playerSeq.length == gameSeq.length)//If the user is finished entering their sequence don't let them enter any extra
             inputPhase = false
         if(playerSeq[playerSeq.length-1] != gameSeq[playerSeq.length-1]){ //if the user enters an incorrect sequence element
@@ -91,18 +97,25 @@ function gameOver(p){
         updateHighScore(score)
         //STRETCH - put a new bubble on the high score and clear it when the next game starts
     }
-    setTimeout(() => {sb.disabled = false})
+    setTimeout(() => {sb.disabled = false}, 1000)
 }
 
-function suttonShow(g){
+function suttonShow(p){ //param g should be a pad div
     //TODO - fade 3 pads & brighten the next in the pattern
     //pads[g]
-    return
+    ///console.log(`starting suttonShow for button ${g}`)
+    return new Promise(resolve => {
+        p.classList.add("bright") //pad immediately flashes white
+        setTimeout(() => { //after 0.75sec the pad resets
+            p.classList.remove("bright")
+            setTimeout(() => {resolve('shown')},250) //after 0.25sec promise is returned, leaving enough time to differentiate between pads sequentially lighting up
+            //console.log('shown  promise is complete')
+        }, 750)
+    })
 }
 
 function suttonSolid(){
-    //TODO - return all pads to default transparency & reset their text(to remove game over description)
-    return
+    //TODO - since suttonShow resets pad brightness now, prepare a function to highlight pads during the winn/loss condition and display text for a time
 }
 
 function updateStreak(s){
