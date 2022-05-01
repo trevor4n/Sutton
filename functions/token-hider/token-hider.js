460c72b74468d4baf40a526cb93b3458e5067ed3
trevor4n <trevor4n@gmail.com>
2022-05-01 19:51:18 -0400

    revisions to the token-hider api

--- functions/token-hider/token-hider.js
+++ functions/token-hider/token-hider.js
@@ -4,24 +4,25 @@ const axios = require('axios')
 const qs = require('qs')
 
 exports.handler = async function (event, context, callback) {
-  const API_PARAMS = qs.stringify(event.queryStringParameters) || '90648782'
-  console.log('API_PARAMS', API_PARAMS)
+  const API_PARAMS = qs.stringify(event.queryStringParameters)
+  const COLLECTION_ID = API_PARAMS.slice(API_PARAMS.indexOf('=') + 1) || '90648782'
   const { API_TOKEN, API_TOKEN_TESTING, API_URL } = process.env
   const paginationCap = '30' //default is 10
 /*   $ netlify functions-create
   ◈ Injected .env file env var: UNSPLASH_KEY
   ◈ Injected .env file env var: UNSPLASH_TESTING_KEY
   note this function requires API_URL and API_TOKEN build environment variables set in your Netlify Site. */
-  const URL = `${API_URL}${API_PARAMS}/photos?client_id=${API_TOKEN}&per_page=${paginationCap}&page=1`
+  // const URL = `${API_URL}${API_PARAMS}/photos?client_id=${API_TOKEN}&per_page=${paginationCap}&page=1`
+  const URL = `${API_URL}${COLLECTION_ID}/photos?client_id=${process.env.UNSPLASH_KEY}&per_page=${paginationCap}&page=1`
   console.log('Constructed URL is ...', URL)
 
   try {
     const { data } = await axios.get(
       URL,
       { headers: {
-        Accept: "application/json"
-        // 'Authorization': 'client_id=' + API_TOKEN,
-        // 'Accept-Version': 'v1'
+        // Accept: "application/json",
+        'Authorization': 'client_id=' + process.env.UNSPLASH_KEY,
+        'Accept-Version': 'v1'
       } }
     )
 
