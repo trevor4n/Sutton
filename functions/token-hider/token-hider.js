ab60f818c6257c22c44883a4ca8a8c189e0501fd
trevor4n <trevor4n@gmail.com>
2022-05-03 14:09:22 -0400

    revision: netlify serverless function

--- functions/token-hider/token-hider.js
+++ functions/token-hider/token-hider.js
@@ -7,15 +7,10 @@ exports.handler = async function (event, context, callback) {
   const API_PARAMS = qs.stringify(event.queryStringParameters)
   const COLLECTION_ID = API_PARAMS.slice(API_PARAMS.indexOf('=') + 1) || '90648782'
   const paginationCap = '30' //default is 10
-  const URL = ''
-  const { NODE_ENV, API_KEY: UNSPLASH_TOKEN, API_TST_KEY: UNSPLASH_TESTING_TOKEN, API_URL: UNSPLASH_URL } = process.env
+  // const { NODE_ENV, API_KEY: UNSPLASH_TOKEN, API_TST_KEY: UNSPLASH_TESTING_TOKEN, API_URL: UNSPLASH_URL } = process.env
   // ◈◈◈◈◈◈◈◈◈◈
-  if (NODE_ENV === 'development') {
-    URL = `${API_URL}${COLLECTION_ID}/photos?client_id=${API_KEY}&per_page=${paginationCap}&page=1`
-  }
-  if (NODE_ENV === 'production') {
-    URL = `${process.env.UNSPLASH_URL}${COLLECTION_ID}/photos?client_id=${process.env.UNSPLASH_TOKEN}&per_page=${paginationCap}&page=1`
-  }
+
+  let URL = `${process.env.UNSPLASH_URL}${COLLECTION_ID}/photos?client_id=${process.env.UNSPLASH_TOKEN}&per_page=${paginationCap}&page=1`
   
   console.log('Constructed URL is ...', URL)
 
